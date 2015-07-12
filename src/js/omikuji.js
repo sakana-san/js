fca1940ea57d82fb47de74613de6d8f1d46f0004
sakana-san <masaru.u1209@gmail.com>
2015-07-12 21:21:04 +0900

    changeStyle file

--- src/js/omikuji.js
+++ src/js/omikuji.js
@@ -1,20 +1,15 @@
 (function(){
-	'use strict';
-	document.getElementById('js-btn').addEventListener('click',function(){
-		var results = ['大吉','小吉','中吉','凶'];
+	var btn = document.getElementById('js-btn');
+	var resultName =  document.getElementById('js-result');
+	btn.addEventListener('click',function(){
+		var results = ['大吉','中吉','小吉','凶','吉'];
 		var result = Math.floor(Math.random() * results.length);
-		document.getElementById('js-result').innerHTML = results[result];
+		resultName.innerHTML = results[result];
 	});
-	document.getElementById('js-btn').addEventListener('mousedown',function(){
+	btn.addEventListener('mousedown',function(){
 		this.className = 'pushed';
 	});
-	document.getElementById('js-btn').addEventListener('mouseup',function(){
+	btn.addEventListener('mouseup',function(){
 		this.className = 'btn';
 	});
 })();
-
-//乱数表示
-//Math.floor(Math.random() * 3);
-
-//配列の個数を表す
-//length
