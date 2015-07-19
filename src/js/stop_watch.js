057258d6560ccc325b96420738a514544938200b
sakana-san <masaru.u1209@gmail.com>
2015-07-19 21:02:28 +0900

    modify and add file

---
+++ src/js/stop_watch.js
@@ -0,0 +1,53 @@
+(function(){
+	'use strict';
+	var startTime;
+	var timerId;
+	var elapsedTime = 0;
+	var isRunning = false;
+
+
+	var startBtn = document.getElementById('s-js-start');
+	var stopBtn = document.getElementById('s-js-stop');
+	var resetBtn = document.getElementById('s-js-reset');
+	var timerTxt = document.getElementById('s-js-timerTxt');
+
+	function setBtnState(start,stop,reset) {
+		startBtn.className = start ? 's-btn is-active' : 's-btn is-in-active';
+		stopBtn.className = stop ? 's-btn is-active' : 's-btn is-in-active';
+		resetBtn.className = reset ? 's-btn is-active' : 's-btn is-in-active';
+	}
+
+	setBtnState(true, false, false);
+
+
+	startBtn.addEventListener('click', function() {
+		if(isRunning) return;
+		isRunning = true;
+		startTime = Date.now();
+		updateTimerText();
+		setBtnState(false, true, false);
+	});
+
+	stopBtn.addEventListener('click', function() {
+		if(!isRunning) return;
+		isRunning = false;
+		elapsedTime += Date.now() - startTime;
+		clearTimeout(timerId);
+		setBtnState(true, false, true);
+	});
+
+	resetBtn.addEventListener('click', function() {
+		if(isRunning) return;
+		timerTxt.innerHTML = '0.00';
+		elapsedTime = 0;
+		setBtnState(true, false, false);
+	});
+
+	function updateTimerText(){
+		timerId = setTimeout(function(){
+			var t = Date.now() - startTime + elapsedTime;
+			timerTxt.innerHTML = (t / 1000).toFixed(2);
+			updateTimerText();
+		}, 10);
+	}
+})();
