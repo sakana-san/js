8a6e448f15c2f4375d2bb43fc29ed64da18021e5
sakana-san <masaru.u1209@gmail.com>
2015-07-27 22:44:10 +0900

    modifiedfile

--- src/js/stop_watch.js
+++ src/js/stop_watch.js
@@ -1,27 +1,43 @@
-(function(){
-	'use strict';
+(function() {
+	'user strict';
 	var startTime;
 	var timerId;
 	var elapsedTime = 0;
+	var isRunning = false; //タイマーの状態を保持 (初期状態:false)
+	var startBtn = document.getElementById('s-js-start');
+	var stopBtn = document.getElementById('s-js-stop');
+	var resetBtn = document.getElementById('s-js-reset');
+	var timerTxt = document.getElementById('s-js-timerTxt');
 
-	var startBtn =document.getElementById('s-js-start');
-	var stopBtn =document.getElementById('s-js-stop');
-	var resetBtn =document.getElementById('s-js-reset');
-	var timerTxt =document.getElementById('s-js-timerTxt');
+	function setBtnState(start, stop, reset)  {
+		startBtn.className = start ? 's-btn is-active' : 's-btn is-in-active';
+		stopBtn.className = stop ? 's-btn is-active' : 's-btn is-in-active';
+		resetBtn.className = reset ? 's-btn is-active' : 's-btn is-in-active';
+	}
+
+	setBtnState(true, false, false);
 
 	startBtn.addEventListener('click', function() {
+		if(isRunning) return; //もしタイマーが走っていたら二重起動したくないので、ここで処理を止める。returnを指定するとこれ以降の処理が実行されない。
+		isRunning = true;
 		startTime = Date.now();
 		updateTimerTxt();
+		setBtnState(false, true, false);
 	});
 
 	stopBtn.addEventListener('click', function() {
-		elapsedTime += Date.now() - startTime;
+		if(!isRunning) return; //タイマーが止まっている時に何もして欲しくない。
+		isRunning = false;
 		clearTimeout(timerId);
+		elapsedTime += Date.now() - startTime;
+		setBtnState(true, false, true);
 	});
 
 	resetBtn.addEventListener('click', function() {
+		if(isRunning) return;//タイマーが走っている時に何もして欲しくない。
 		timerTxt.innerHTML = '0.00';
 		elapsedTime = 0;
+		setBtnState(true, false, false);
 	});
 
 	function updateTimerTxt() {
@@ -29,11 +45,9 @@
 			var t = Date.now() - startTime + elapsedTime;
 			timerTxt.innerHTML = (t / 1000).toFixed(2);
 			updateTimerTxt();
-		});
+		}, 10);
 	}
-
-})(); 
-
+})();
 /*
 結合代入演算子
 var a = 0;
@@ -47,7 +61,7 @@ console.log(a);
 	var startTime;
 	var timerId;
 	var elapsedTime = 0;
-	var isRunning = false;
+	var isRunning = false;//タイマーの状態を保持 (初期状態:false)
 
 
 	var startBtn = document.getElementById('s-js-start');
@@ -65,7 +79,7 @@ console.log(a);
 
 
 	startBtn.addEventListener('click', function() {
-		if(isRunning) return;
+		if(isRunning) return; //もしタイマーが走っていたら二重起動したくないので、ここで処理を止める。returnを指定するとこれ以降の処理が実行されない。
 		isRunning = true;
 		startTime = Date.now();
 		updateTimerText();
@@ -73,7 +87,7 @@ console.log(a);
 	});
 
 	stopBtn.addEventListener('click', function() {
-		if(!isRunning) return;
+		if(!isRunning) return; //タイマーが止まっている時に何もして欲しくない。
 		isRunning = false;
 		elapsedTime += Date.now() - startTime;
 		clearTimeout(timerId);
