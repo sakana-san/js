ffc6177353e8265fc578f4fa3a0415ea9c350ef7
masaru uozaki <sakana@uozakimasaru-no-MacBook-Pro.local>
2015-11-02 12:06:22 +0900

    修正

--- src/js/concat/test.js
+++ src/js/concat/test.js
@@ -42,3 +42,49 @@ $(function() {
 	});
 */
 });
+/*
+(function(name) {
+	console.log('hello' + name);
+})('長澤まさみ');
+var i = 0;
+*/
+/*
+var i = 0;
+function show() {
+	console.log(i++);
+	var timer = setTimeout(function() {
+		show();
+	}, 1000);
+	if(i > 3) {
+		clearTimeout(timer);
+	}
+}
+show();
+*/
+
+//オブジェクト
+/*
+var user = {
+	email: 'taguchi@gmail.com',
+	score: 80,
+	greet: function(name) {
+		console.log('hello' + name + 'from' + this.email);
+	}
+};
+*/
+var koukouyakyu = {
+	name: '大阪桐蔭',
+	power: 90,
+	protect: 85,
+	strength: 95,
+	greet: function(senbatu) {
+		console.log('今年の' + senbatu + this.name + ' が出場します')
+	}
+};
+koukouyakyu.greet('選抜');
+//console.log(koukouyakyu.name + koukouyakyu.power);
+//console.log(user['email']);
+//console.log(user.email);
+//user.score = 100;
+//console.log(user);
+//user.greet('Tom');
