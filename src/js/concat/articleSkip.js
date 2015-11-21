32286e8ef9538b3138c68b9c62265dc83041785d
masaru uozaki <sakana@uozakimasaru-no-MacBook-Pro.local>
2015-11-21 13:19:53 +0900

    綴りを間違っていたので修正した

--- src/js/concat/articleSkip.js
+++ src/js/concat/articleSkip.js
@@ -1,12 +1,11 @@
-
 //aタグを使って任意の場所にスクロール
 $(function() {
 	$('.anc').on('click', function() {
 		var url = this.href;
 		var part = url.split('#');
 		var target = part[1];
-		var tagetTop = $('#' + target).offset().top;
-		$('body,html').animate({scrollTop: tagetTop}, 1000);
+		var targetTop = $('#' + target).offset().top;
+		$('body,html').animate({scrollTop: targetTop}, 1000);
 		return false;
 	});
 });
