98fd2cb4391661fc17dbf839acda0174cb392e62
masaru uozaki <sakana@uozakimasaru-no-MacBook-Pro.local>
2015-11-22 18:40:43 +0900

    modalを実装しました

---
+++ src/js/concat/modal.js
@@ -0,0 +1,30 @@
+$(function() {
+	$('.p-modalOpen').on('click', function() {
+		//キーボード操作などにより、オーバーレイが多重起動するのを防止する
+		$(this).blur();
+		if($('.p-modalOverlay')[0]) return false;
+
+		$('.p-modal').append('<div class="p-modalOverlay"></div>');
+		$('.p-modalOverlay').fadeIn();
+		$('.p-modalContent').fadeIn();
+
+		centeringModal();
+
+		$('.p-modalOverlay, .c-btnModalClose').on('unbind click', function() {
+			$('.p-modalContent, .p-modalOverlay').fadeOut(function() {
+				$('.p-modalOverlay').remove();
+			});
+		});
+	});
+
+	function centeringModal() {
+		var width = $(window).width();
+		var height = $(window).height();
+		var contentWidth = $('.p-modalContent').outerWidth();
+		var contentHeight = $('.p-modalContent').outerHeight();
+		var pxLeft = ((width - contentWidth) / 2);
+		var pxTop = ((height - contentHeight) / 2);
+
+		$('.p-modalContent').css({'left': pxLeft + 'px', 'top': pxTop + 'px'});
+	}
+});
